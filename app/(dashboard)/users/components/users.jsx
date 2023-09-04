e68556a35f6953864e2ad59d4926b377b2db0f9e
Jo <jose_f_dirazar@hotmail.com>
2023-09-03 21:49:52 -0300

    bann users

--- app/(dashboard)/users/components/users.jsx
+++ app/(dashboard)/users/components/users.jsx
@@ -1,17 +1,48 @@
-import axios from 'axios'
-export default async function FetchUsers() {
-    const { data }= await axios.get('http://localhost:3000/api/users')
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-    return (
-        <>
-            {data?.map((user, index) => (
-                <div key={index} className='flex items-center pt-2 pb-2 border-t-2 border-slate-500'>
-                    <div className='flex justify-center w-60'>{user.email}</div>
-                    <div className='flex justify-center w-96'>{user.id}</div>
-                    <div className='flex justify-center w-48'>{user.name} {user.last_name}</div>
-                    <button onClick='a Cambiar' className='h-10 cursor-pointer ml-10 bg-red-500 rounded-[4px] pl-1 pr-1'>Disable account</button>
-                </div>
-            ))}
-        </>
-    )
+export default function FetchUsers() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get("http://localhost:3000/api/users");
+
+      setUsers([...data]);
+    })();
+  }, []);
+
+  async function handleBann(hyperEventUserId) {
+    try {
+        console.log(hyperEventUserId)
+        await axios.patch("http://localhost:3000/api/users", { hyperEventUserId });
+        
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  return (
+    <>
+      {users?.map((user, index) => (
+        <div
+          key={index}
+          className="flex items-center pt-2 pb-2 border-t-2 border-slate-500"
+        >
+          <div className="flex justify-center w-60">{user.email}</div>
+          <div className="flex justify-center w-96">{user.id}</div>
+          <div className="flex justify-center w-48">
+            {user.name} {user.last_name}
+          </div>
+          <button
+            onClick={() => handleBann(user.id)}
+            className="h-10 cursor-pointer ml-10 bg-red-500 rounded-[4px] pl-1 pr-1"
+          >
+            Bann
+          </button>
+        </div>
+      ))}
+    </>
+  );
 }
--- app/api/users/route.js
+++ app/api/users/route.js
@@ -3,16 +3,39 @@ import { auth } from "@clerk/nextjs";
 
 import prismadb from "../../../lib/prismadb";
 
+export async function GET(req) {
+  try {
+    const userId = auth();
 
-export async function GET(
-    req
-) { 
-    try {
-        const users = await prismadb.user.findMany()
-       
-        return NextResponse.json(users)
-    } catch (error) {
-        console.log('USERS_GET', error)
-        return new NextResponse("Internal error", { status: 500 });
-    }
+    if (!userId) return new NextResponse("Unauthenticated", { status: 401 });
+
+    const users = await prismadb.user.findMany();
+
+    return NextResponse.json(users);
+  } catch (error) {
+    console.log("USERS_GET", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function PATCH(req) {
+  try {
+    
+    const userId = auth();
+    if (!userId) return new NextResponse("Unauthenticated", { status: 401 });
+
+    const body = await req.json();
+    const { hyperEventUserId } = body;
+
+    await prismadb.user.update({
+        where: {
+            id: hyperEventUserId
+        },
+        data: {
+            admin: true
+        }
+    })
+
+    return new NextResponse("User banned.", { status: 200 })
+  } catch (error) {}
 }
