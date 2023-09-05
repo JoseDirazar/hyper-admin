b0693ab872ac7c98b21eaf419e0ea9bb22279d77
Jo <jose_f_dirazar@hotmail.com>
2023-09-05 19:15:01 -0300

    sadf

--- app/(dashboard)/events/components/handleEvent.jsx
+++ app/(dashboard)/events/components/handleEvent.jsx
@@ -4,33 +4,45 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function HandleEvent({ eventStatus, eventId }) {
-  
   const [status, setStatus] = useState(true);
 
   useEffect(() => {
-    setStatus(eventStatus)
-  }, [])
+    setStatus(eventStatus);
+  }, []);
 
   async function handleStatus(statusParam, eventIdParam) {
     try {
-      const statusResponse = await axios.patch('http://localhost:3000/api/events', {
-        eventStatus: statusParam,
-        eventId: eventIdParam,
-      });
-      console.log("status response: ", statusResponse.data)
+      const statusResponse = await axios.patch(
+        "http://localhost:3000/api/events",
+        {
+          eventStatus: statusParam,
+          eventId: eventIdParam,
+        }
+      );
+      console.log("status response: ", statusResponse.data);
       setStatus(statusResponse.data);
     } catch (error) {
-     console.log(error)
+      console.log(error);
     }
   }
-  
+
   return (
-    <>
+    <div key={eventId}>
       {status ? (
-        <button className="bg-red-600 rounded-full border-solid p-2" onClick={() => handleStatus(false, eventId)}>Disable</button>
+        <button
+          className="bg-red-600 rounded-full border-solid p-2"
+          onClick={() => handleStatus(false, eventId)}
+        >
+          Disable
+        </button>
       ) : (
-        <button className="bg-blue-600 rounded-full border-solid p-2" onClick={() => handleStatus(true, eventId)}>Enable</button>
+        <button
+          className="bg-blue-600 rounded-full border-solid p-2"
+          onClick={() => handleStatus(true, eventId)}
+        >
+          Enable
+        </button>
       )}
-    </>
+    </div>
   );
 }
--- app/(dashboard)/events/page.jsx
+++ app/(dashboard)/events/page.jsx
@@ -1,28 +1,72 @@
+'use client'
+
 import axios from "axios";
 import HandleEvent from "./components/handleEvent";
 import Image from "next/image";
-const EventsPage = async () => {
-  let {data} = await axios.get("http://localhost:3000/api/events")
-  
+import { useEffect, useState } from "react";
+
+
+const EventsPage = () => {
+
+
+  const [events, setEvents] = useState([])
+
+
+  useEffect(() => {
+    (async () => {
+      try {
+        let { data } = await axios.get("http://localhost:3000/api/events");
+        setEvents([...data])
+      } catch (error) {
+        console.log(error)
+      }
+    })()
+  }, [events])
+
+
   return (
-    <div key={2838123} className="flex flex-row flex-wrap h-full">
-      {data?.map((event, index) => (
-        <div
-          kay={index}
-          className="flex flex-col h-80 w-80 items-center justify-center gap-2 border-2 border-b-rose-800 "
-        >
-          
-          <p className="text-sm">{event.event_name}</p>
-          <Image
-            className="w-auto h-auto"
-            src={event.event_image}
-            width={200}
-            height={200}
-            alt={event.event_name}
-          />
-          <HandleEvent eventStatus={event.active} eventId={event.id} />
+    <div className="flex flex-row h-full">
+      <div className="flex flex-col w-[50%]">
+        <div key={2838123} className="flex flex-row flex-wrap ">
+          {events?.filter((event) => event.active === true).map((event, index) => (
+            <div
+              key={index}
+              className="flex flex-col h-80 w-80 items-center justify-center gap-2 border-2 border-blue-500 "
+            >
+              <p className="text-sm">{event.event_name}</p>
+              <Image
+                className="w-auto h-auto"
+                src={event.event_image}
+                width={200}
+                height={200}
+                alt={event.event_name}
+              />
+              <HandleEvent eventStatus={event.active} eventId={event.id} />
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
+      <div className="flex flex-col w-[50%]">
+        <div key={283812123} className="flex flex-row flex-wrap ">
+          
+          {events?.filter((event) => event.active === false).map((event, index) => (
+            <div
+              key={index}
+              className="flex flex-col h-80 w-80 items-center justify-center gap-2 border-2 border-b-rose-800 "
+            >
+              <p className="text-sm">{event.event_name}</p>
+              <Image
+                className="w-auto h-auto"
+                src={event.event_image}
+                width={200}
+                height={200}
+                alt={event.event_name}
+              />
+              <HandleEvent eventStatus={event.active} eventId={event.id} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
